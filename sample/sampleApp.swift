05c9518f8a51c5456bf6037855b60de5ea5a5a79
Trance <xp46290@gmail.com>
2023-04-18 19:22:19 -0400

    Initial Commit

---
+++ sample/sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampleApp.swift
+//  sample
+//
+//  Created by Mark Trance on 4/18/23.
+//
+
+import SwiftUI
+
+@main
+struct sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
